123217060807a95f747b472714ae9a35a4447076
Giles Williams <aegidian@oolite.org>
2006-08-10 09:50:01 +0000

    baby steps - the changes necessary for GLSL in SDL
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@719 127b21dd-08f5-0310-b4b7-95ae10353056

--- src/Core/OOOpenGL.h
+++ src/Core/OOOpenGL.h
@@ -2,6 +2,7 @@
 
 #ifndef GNUSTEP
 
+// Apple OpenGL includes...
 #include <OpenGL/OpenGL.h>
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
@@ -9,6 +10,15 @@
 
 #else
 
+// SDL OpenGL includes...
+
+// prevent the including of SDL_opengl.h loading a previous version of glext.h
+#define NO_SDL_GLEXT
+
+// the standard SDL_opengl.h
 #include "SDL_opengl.h"
+
+// include an up-to-date version of glext.h
+#include "glext.h"
 
 #endif
