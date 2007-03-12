a0f6153086149c689b64ff02fb1c7186621d653b
Jens Ayton <jens@oolite.org>
2007-03-12 13:26:41 +0000

    Added OOCollectionExtractors, a cleaner alternative to all those if ([dict objectForKey:key]) foo = [dict objectForKey:key]; else foo = someDefault; cases (and array equivalents).
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@855 127b21dd-08f5-0310-b4b7-95ae10353056

---
+++ src/Core/OOCollectionExtractors.h
@@ -0,0 +1,80 @@
+/*
+
+OOCollectionExtractors.h
+
+Convenience extensions to Foundation collections to extract optional values.
+
+Note on types: ideally, stdint.h types would be used for integers. However,
+NSNumber doesn't do this, so doing so portably would add new complications.
+
+Oolite
+Copyright (C) 2004-2007 Giles C Williams and contributors
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+
+*/
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSArray (OOExtractor)
+
+- (char)charAtIndex:(unsigned)index defaultValue:(char)value;
+- (short)shortAtIndex:(unsigned)index defaultValue:(short)value;
+- (int)intAtIndex:(unsigned)index defaultValue:(int)value;
+- (long)longAtIndex:(unsigned)index defaultValue:(long)value;
+- (long long)longLongAtIndex:(unsigned)index defaultValue:(long long)value;
+
+- (unsigned char)unsignedCharAtIndex:(unsigned)index defaultValue:(unsigned char)value;
+- (unsigned short)unsignedShortAtIndex:(unsigned)index defaultValue:(unsigned short)value;
+- (unsigned int)unsignedIntAtIndex:(unsigned)index defaultValue:(unsigned int)value;
+- (unsigned long)unsignedLongAtIndex:(unsigned)index defaultValue:(unsigned long)value;
+- (unsigned long long)unsignedLongLongAtIndex:(unsigned)index defaultValue:(unsigned long long)value;
+
+- (BOOL)boolAtIndex:(unsigned)index defaultValue:(BOOL)value;
+
+- (float)floatAtIndex:(unsigned)index defaultValue:(float)value;
+- (double)doubleAtIndex:(unsigned)index defaultValue:(double)value;
+
+- (id)objectAtIndex:(unsigned)index defaultValue:(id)value;
+- (NSString *)stringAtIndex:(unsigned)index defaultValue:(NSString *)value;
+
+@end
+
+
+@interface NSDictionary (OOExtractor)
+
+- (char)charForKey:(id)key defaultValue:(char)value;
+- (short)shortForKey:(id)key defaultValue:(short)value;
+- (int)intForKey:(id)key defaultValue:(int)value;
+- (long)longForKey:(id)key defaultValue:(long)value;
+- (long long)longLongForKey:(id)key defaultValue:(long long)value;
+
+- (unsigned char)unsignedCharForKey:(id)key defaultValue:(unsigned char)value;
+- (unsigned short)unsignedShortForKey:(id)key defaultValue:(unsigned short)value;
+- (unsigned int)unsignedIntForKey:(id)key defaultValue:(unsigned int)value;
+- (unsigned long)unsignedLongForKey:(id)key defaultValue:(unsigned long)value;
+- (unsigned long long)unsignedLongLongForKey:(id)key defaultValue:(unsigned long long)value;
+
+- (BOOL)boolForKey:(id)key defaultValue:(BOOL)value;
+
+- (float)floatForKey:(id)key defaultValue:(float)value;
+- (double)doubleForKey:(id)key defaultValue:(double)value;
+
+- (id)objectForKey:(id)key defaultValue:(id)value;
+- (NSString *)stringForKey:(id)key defaultValue:(NSString *)value;
+
+@end
---
+++ src/Core/OOCollectionExtractors.m
@@ -0,0 +1,559 @@
+/*
+
+OOCollectionExtractors.m
+
+Oolite
+Copyright (C) 2004-2007 Giles C Williams and contributors
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+
+*/
+
+#import "OOCollectionExtractors.h"
+
+
+@implementation NSArray (OOExtractor)
+
+- (char)charAtIndex:(unsigned)index defaultValue:(char)value
+{
+	id					objVal = [self objectAtIndex:index];
+	int					intVal;
+	char				result;
+	
+	if ([objVal respondsToSelector:@selector(charValue)])  result = [objVal charValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < CHAR_MIN) intVal = CHAR_MIN;
+		else if (CHAR_MAX < intVal) intVal = CHAR_MAX;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (short)shortAtIndex:(unsigned)index defaultValue:(short)value
+{
+	id					objVal = [self objectAtIndex:index];
+	int					intVal;
+	short				result;
+	
+	if ([objVal respondsToSelector:@selector(shortValue)])  result = [objVal shortValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < SHRT_MIN) intVal = SHRT_MIN;
+		else if (SHRT_MAX < intVal) intVal = SHRT_MAX;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (int)intAtIndex:(unsigned)index defaultValue:(int)value
+{
+	id					objVal = [self objectAtIndex:index];
+	int					result;
+	
+	if ([objVal respondsToSelector:@selector(intValue)])  result = [objVal intValue];
+	else result = value;
+
+	return result;
+}
+
+
+- (long)longAtIndex:(unsigned)index defaultValue:(long)value
+{
+	id					objVal = [self objectAtIndex:index];
+	long				result;
+	
+	if ([objVal respondsToSelector:@selector(longValue)])  result = [objVal longValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])  result = [objVal intValue];
+	else result = value;
+
+	return result;
+}
+
+
+- (long long)longLongAtIndex:(unsigned)index defaultValue:(long long)value
+{
+	id					objVal = [self objectAtIndex:index];
+	long long			result;
+	
+	if ([objVal respondsToSelector:@selector(longLongValue)])  result = [objVal longLongValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])  result = [objVal intValue];
+	else result = value;
+
+	return result;
+}
+
+
+- (unsigned char)unsignedCharAtIndex:(unsigned)index defaultValue:(unsigned char)value
+{
+	id					objVal = [self objectAtIndex:index];
+	int					intVal;
+	unsigned char		result;
+	
+	if ([objVal respondsToSelector:@selector(unsignedCharValue)])  result = [objVal unsignedCharValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < 0) intVal = 0;
+		else if (UCHAR_MAX < intVal) intVal = UCHAR_MAX;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (unsigned short)unsignedShortAtIndex:(unsigned)index defaultValue:(unsigned short)value
+{
+	id					objVal = [self objectAtIndex:index];
+	int					intVal;
+	unsigned short		result;
+	
+	if ([objVal respondsToSelector:@selector(unsignedShortValue)])  result = [objVal unsignedShortValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < 0) intVal = 0;
+		else if (USHRT_MAX < intVal) intVal = USHRT_MAX;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (unsigned int)unsignedIntAtIndex:(unsigned)index defaultValue:(unsigned int)value
+{
+	id					objVal = [self objectAtIndex:index];
+	int					intVal;
+	unsigned int		result;
+	
+	if ([objVal respondsToSelector:@selector(unsignedIntValue)])  result = [objVal unsignedIntValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < 0) intVal = 0;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (unsigned long)unsignedLongAtIndex:(unsigned)index defaultValue:(unsigned long)value
+{
+	id					objVal = [self objectAtIndex:index];
+	int					intVal;
+	unsigned long		result;
+	
+	if ([objVal respondsToSelector:@selector(unsignedLongValue)])  result = [objVal unsignedLongValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < 0) intVal = 0;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (unsigned long long)unsignedLongLongAtIndex:(unsigned)index defaultValue:(unsigned long long)value
+{
+	id					objVal = [self objectAtIndex:index];
+	int					intVal;
+	unsigned long long	result;
+	
+	if ([objVal respondsToSelector:@selector(unsignedLongLongValue)])  result = [objVal unsignedLongLongValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < 0) intVal = 0;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (BOOL)boolAtIndex:(unsigned)index defaultValue:(BOOL)value
+{
+	id					objVal = [self objectAtIndex:index];
+	BOOL				result;
+	
+	if ([objVal isKindOfClass:[NSString class]])
+	{
+		// This is here because A. [NSString boolValue] exists, but is not documented; and B. we want to return the default value (rather than NO) if the string doesn't make sense as a boolean.
+		if (NSOrderedSame == [objVal caseInsensitiveCompare:@"yes"] ||
+			NSOrderedSame == [objVal caseInsensitiveCompare:@"true"] ||
+			NSOrderedSame == [objVal caseInsensitiveCompare:@"on"] ||
+			[objVal intValue] != 0)
+		{
+			result = YES;
+		}
+		else if (NSOrderedSame == [objVal caseInsensitiveCompare:@"no"] ||
+				 NSOrderedSame == [objVal caseInsensitiveCompare:@"false"] ||
+				 NSOrderedSame == [objVal caseInsensitiveCompare:@"off"] ||
+				 NSOrderedSame == [objVal caseInsensitiveCompare:@"0"] ||
+				 NSOrderedSame == [objVal caseInsensitiveCompare:@"-0"])
+		{
+			result = NO;
+		}
+		else result = value;
+	}
+	else if ([objVal respondsToSelector:@selector(boolValue)])  result = [objVal boolValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		result = [objVal intValue] != 0;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (float)floatAtIndex:(unsigned)index defaultValue:(float)value
+{
+	id					objVal = [self objectAtIndex:index];
+	float				result;
+	
+	if ([objVal respondsToSelector:@selector(floatValue)])  result = [objVal floatValue];
+	else if ([objVal respondsToSelector:@selector(doubleValue)])  result = [objVal doubleValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])  result = [objVal intValue];
+	else result = value;
+
+	return result;
+}
+
+
+- (double)doubleAtIndex:(unsigned)index defaultValue:(double)value
+{
+	id					objVal = [self objectAtIndex:index];
+	double				result;
+	
+	if ([objVal respondsToSelector:@selector(doubleValue)])  result = [objVal doubleValue];
+	else if ([objVal respondsToSelector:@selector(floatValue)])  result = [objVal floatValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])  result = [objVal intValue];
+	else result = value;
+
+	return result;
+}
+
+
+- (id)objectAtIndex:(unsigned)index defaultValue:(id)value
+{
+	id					objVal = [self objectAtIndex:index];
+	id					result;
+	
+	if (objVal != nil) result = objVal;
+	else result = value;
+	
+	return result;
+}
+
+
+- (NSString *)stringAtIndex:(unsigned)index defaultValue:(NSString *)value
+{
+	id					objVal = [self objectAtIndex:index];
+	id					result;
+	
+	if ([objVal isKindOfClass:[NSString class]]) result = objVal;
+	else result = value;
+	
+	return result;
+}
+
+@end
+
+
+@implementation NSDictionary (OOExtractor)
+
+- (char)charForKey:(id)key defaultValue:(char)value
+{
+	id					objVal = [self objectForKey:key];
+	int					intVal;
+	char				result;
+	
+	if ([objVal respondsToSelector:@selector(charValue)])  result = [objVal charValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < CHAR_MIN) intVal = CHAR_MIN;
+		else if (CHAR_MAX < intVal) intVal = CHAR_MAX;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (short)shortForKey:(id)key defaultValue:(short)value
+{
+	id					objVal = [self objectForKey:key];
+	int					intVal;
+	short				result;
+	
+	if ([objVal respondsToSelector:@selector(shortValue)])  result = [objVal shortValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < SHRT_MIN) intVal = SHRT_MIN;
+		else if (SHRT_MAX < intVal) intVal = SHRT_MAX;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (int)intForKey:(id)key defaultValue:(int)value
+{
+	id					objVal = [self objectForKey:key];
+	int					result;
+	
+	if ([objVal respondsToSelector:@selector(intValue)])  result = [objVal intValue];
+	else result = value;
+
+	return result;
+}
+
+
+- (long)longForKey:(id)key defaultValue:(long)value
+{
+	id					objVal = [self objectForKey:key];
+	long				result;
+	
+	if ([objVal respondsToSelector:@selector(longValue)])  result = [objVal longValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])  result = [objVal intValue];
+	else result = value;
+
+	return result;
+}
+
+
+- (long long)longLongForKey:(id)key defaultValue:(long long)value
+{
+	id					objVal = [self objectForKey:key];
+	long long			result;
+	
+	if ([objVal respondsToSelector:@selector(longLongValue)])  result = [objVal longLongValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])  result = [objVal intValue];
+	else result = value;
+
+	return result;
+}
+
+
+- (unsigned char)unsignedCharForKey:(id)key defaultValue:(unsigned char)value
+{
+	id					objVal = [self objectForKey:key];
+	int					intVal;
+	unsigned char		result;
+	
+	if ([objVal respondsToSelector:@selector(unsignedCharValue)])  result = [objVal unsignedCharValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < 0) intVal = 0;
+		else if (UCHAR_MAX < intVal) intVal = UCHAR_MAX;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (unsigned short)unsignedShortForKey:(id)key defaultValue:(unsigned short)value
+{
+	id					objVal = [self objectForKey:key];
+	int					intVal;
+	unsigned short		result;
+	
+	if ([objVal respondsToSelector:@selector(unsignedShortValue)])  result = [objVal unsignedShortValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < 0) intVal = 0;
+		else if (USHRT_MAX < intVal) intVal = USHRT_MAX;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (unsigned int)unsignedIntForKey:(id)key defaultValue:(unsigned int)value
+{
+	id					objVal = [self objectForKey:key];
+	int					intVal;
+	unsigned int		result;
+	
+	if ([objVal respondsToSelector:@selector(unsignedIntValue)])  result = [objVal unsignedIntValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < 0) intVal = 0;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (unsigned long)unsignedLongForKey:(id)key defaultValue:(unsigned long)value
+{
+	id					objVal = [self objectForKey:key];
+	int					intVal;
+	unsigned long		result;
+	
+	if ([objVal respondsToSelector:@selector(unsignedLongValue)])  result = [objVal unsignedLongValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < 0) intVal = 0;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (unsigned long long)unsignedLongLongForKey:(id)key defaultValue:(unsigned long long)value
+{
+	id					objVal = [self objectForKey:key];
+	int					intVal;
+	unsigned long long	result;
+	
+	if ([objVal respondsToSelector:@selector(unsignedLongLongValue)])  result = [objVal unsignedLongLongValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		intVal = [objVal intValue];
+		if (intVal < 0) intVal = 0;
+		result = intVal;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (BOOL)boolForKey:(id)key defaultValue:(BOOL)value
+{
+	id					objVal = [self objectForKey:key];
+	BOOL				result;
+	
+	if ([objVal isKindOfClass:[NSString class]])
+	{
+		// This is here because A. [NSString boolValue] exists, but is not documented; and B. we want to return the default value (rather than NO) if the string doesn't make sense as a boolean.
+		if (NSOrderedSame == [objVal caseInsensitiveCompare:@"yes"] ||
+			NSOrderedSame == [objVal caseInsensitiveCompare:@"true"] ||
+			NSOrderedSame == [objVal caseInsensitiveCompare:@"on"] ||
+			[objVal intValue] != 0)
+		{
+			result = YES;
+		}
+		else if (NSOrderedSame == [objVal caseInsensitiveCompare:@"no"] ||
+				 NSOrderedSame == [objVal caseInsensitiveCompare:@"false"] ||
+				 NSOrderedSame == [objVal caseInsensitiveCompare:@"off"] ||
+				 NSOrderedSame == [objVal caseInsensitiveCompare:@"0"] ||
+				 NSOrderedSame == [objVal caseInsensitiveCompare:@"-0"])
+		{
+			result = NO;
+		}
+		else result = value;
+	}
+	else if ([objVal respondsToSelector:@selector(boolValue)])  result = [objVal boolValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])
+	{
+		result = [objVal intValue] != 0;
+	}
+	else result = value;
+
+	return result;
+}
+
+
+- (float)floatForKey:(id)key defaultValue:(float)value
+{
+	id					objVal = [self objectForKey:key];
+	float				result;
+	
+	if ([objVal respondsToSelector:@selector(floatValue)])  result = [objVal floatValue];
+	else if ([objVal respondsToSelector:@selector(doubleValue)])  result = [objVal doubleValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])  result = [objVal intValue];
+	else result = value;
+
+	return result;
+}
+
+
+- (double)doubleForKey:(id)key defaultValue:(double)value
+{
+	id					objVal = [self objectForKey:key];
+	double				result;
+	
+	if ([objVal respondsToSelector:@selector(doubleValue)])  result = [objVal doubleValue];
+	else if ([objVal respondsToSelector:@selector(floatValue)])  result = [objVal floatValue];
+	else if ([objVal respondsToSelector:@selector(intValue)])  result = [objVal intValue];
+	else result = value;
+
+	return result;
+}
+
+
+- (id)objectForKey:(id)key defaultValue:(id)value
+{
+	id					objVal = [self objectForKey:key];
+	id					result;
+	
+	if (objVal != nil) result = objVal;
+	else result = value;
+	
+	return result;
+}
+
+
+- (NSString *)stringForKey:(id)key defaultValue:(NSString *)value
+{
+	id					objVal = [self objectForKey:key];
+	id					result;
+	
+	if ([objVal isKindOfClass:[NSString class]]) result = objVal;
+	else result = value;
+	
+	return result;
+}
+
+@end
