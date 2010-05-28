60fe22388f3b72931f20f254f1b0195ed3ac1cb1
Marc <kaks@oolite.org>
2010-05-28 11:06:07 +0000

    - Oops, thanks A_C!
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@3423 127b21dd-08f5-0310-b4b7-95ae10353056

--- src/Core/Entities/PlayerEntity.m
+++ src/Core/Entities/PlayerEntity.m
@@ -4739,10 +4739,13 @@ static BOOL replacingMissile = NO;
 	gui_screen = GUI_SCREEN_STATUS;
 	BOOL			guiChanged = (oldScreen != gui_screen);
 	
-	// the following two lines are unnecessary at best, and at worst - with overlapping planets - can cause Oolite to refer to the wrong system.
+	// the following line is unnecessary, and - with overlapping planets - can cause Oolite to refer to the wrong system.
 	//system_seed = [UNIVERSE findSystemAtCoords:galaxy_coordinates withGalaxySeed:galaxy_seed];
-	//target_system_seed = [UNIVERSE findSystemAtCoords:cursor_coordinates withGalaxySeed:galaxy_seed];
-
+	if (is_nil_seed(target_system_seed))
+	{
+		target_system_seed = [UNIVERSE findSystemAtCoords:cursor_coordinates withGalaxySeed:galaxy_seed];
+	}
+	
 	systemName = [UNIVERSE inInterstellarSpace] ? DESC(@"interstellar-space") : [UNIVERSE getSystemName:system_seed];
 	if ([self isDocked] && dockedStation != [UNIVERSE station])
 	{
