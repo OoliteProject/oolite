bbfe736af7791f406ea7acf918180de07d8bd346
Jens Ayton <jens@oolite.org>
2010-05-30 15:20:48 +0000

    Here, have some files.
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@3435 127b21dd-08f5-0310-b4b7-95ae10353056

---
+++ src/Core/NSObjectOOExtensions.m
@@ -0,0 +1,61 @@
+/*
+
+OOEntityFilterPredicate.m
+
+
+Copyright (C) 2010 Jens Ayton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#import "NSObjectOOExtensions.h"
+
+
+#if OOLITE_MAC_OS_X
+#if OOLITE_LEOPARD
+#import <objc/runtime.h>
+#else
+#import <objc/objc-class.h>
+#endif
+#endif
+
+
+@implementation NSObject (OOExtensions)
+
+#ifndef NDEBUG
+
++ (size_t) oo_instanceSize
+{
+#if OOLITE_LEOPARD || OOLITE_GNUSTEP_1_20
+	return class_getInstanceSize(self);
+#else
+	return ((Class)self)->instance_size;
+#endif
+}
+
+
+- (size_t) oo_objectSize
+{
+	return [[self class] oo_instanceSize];
+}
+
+#endif
+
+@end
