6bb270521625de66acb28a3fdedecb789ef0613d
Jens Ayton <jens@oolite.org>
2010-06-13 16:00:30 +0000

    Re-added ability to use SystemInfo.systemsInRange() as an instance method.
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@3519 127b21dd-08f5-0310-b4b7-95ae10353056

--- Resources/Scripts/oolite-global-prefix.js
+++ Resources/Scripts/oolite-global-prefix.js
@@ -92,7 +92,23 @@ SystemInfo.systemsInRange = function(range)
 		range = 7;
 	}
 	
+	// Default to using the current system.
 	var thisSystem = system.info;
+	
+	// If called on an instance instead of the SystemInfo constructor, use that system instead.
+	if (this !== SystemInfo)
+	{
+		if (this.systemID !== undefined && this.distanceToSystem !== undefined)
+		{
+			thisSystem = this;
+		}
+		else
+		{
+			special.jsWarning("systemsInRange() called from the wrong context. Returning empty array.");
+			return [];
+		}
+	}
+	
 	return SystemInfo.filteredSystems(this, function(other)
 	{
 		return (other.systemID !== thisSystem.systemID) && (thisSystem.distanceToSystem(other) <= range);
