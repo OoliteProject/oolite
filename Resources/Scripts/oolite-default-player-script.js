65dff53391ecf4f2b13164551c0a68248e8db6f0
Marc <kaks@oolite.org>
2010-07-04 18:39:27 +0000

    - added one missing copyright info, and updated copyright notices in maintenance.
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@3655 127b21dd-08f5-0310-b4b7-95ae10353056

--- Resources/Scripts/oolite-default-player-script.js
+++ Resources/Scripts/oolite-default-player-script.js
@@ -5,10 +5,30 @@ oolite-default-player-script.js
 Default script for the player. It isn’t intended to do anything, merely to
 exist so the player isn’t a special case.
 
+
+Oolite
+Copyright © 2004–2010 Giles C Williams and contributors
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+
 */
 
 
 this.name			= "oolite-default-player-script";
 this.author			= "Jens Ayton";
 this.description	= "Standard script for player ship.";
+this.copyright		= "© 2008–2010 the Oolite team.";
 this.version		= "1.75";
