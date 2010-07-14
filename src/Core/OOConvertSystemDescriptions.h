8328dd81016f7b87bc9fa3d63f6f4e3ca56f35dc
Jens Ayton <jens@oolite.org>
2010-07-14 17:01:24 +0000

    Cleaned up some copyright notices by special request.
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@3668 127b21dd-08f5-0310-b4b7-95ae10353056

--- src/Core/OOConvertSystemDescriptions.h
+++ src/Core/OOConvertSystemDescriptions.h
@@ -1,19 +1,48 @@
+/*
+
+OOConvertSystemDescriptions.m
+Oolite
+
+Functions to convert system descriptions between dictionary and array
+formats.
+
+The array format is used in descriptions.plist. Each set of strings is an
+array, the sets are stored in a master array, and cross-references are
+indices into the master array.
+
+The dictionary format is more human-friendly, with string sets identified
+by name and cross-references using names.
+
+The indicesToKeys parameter is optional; if it is nil or incomplete,
+indices or (index-based) keys will be synthesized.
+
+
+Copyright (C) 2008 Jens Ayton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+
 #import <Foundation/Foundation.h>
 
 
-/*	Functions to convert system descriptions between dictionary and array
-	formats.
-	
-	The array format is used in descriptions.plist. Each set of strings is an
-	array, the sets are stored in a master array, and cross-references are
-	indices into the master array.
-	
-	The dictionary format is more human-friendly, with string sets identified
-	by name and cross-references using names.
-	
-	The indicesToKeys parameter is optional; if it is nil or incomplete,
-	indices or (index-based) keys will be synthesized.
-*/
 NSArray *OOConvertSystemDescriptionsToArrayFormat(NSDictionary *descriptionsInDictionaryFormat, NSDictionary *indicesToKeys);
 NSDictionary *OOConvertSystemDescriptionsToDictionaryFormat(NSArray *descriptionsInArrayFormat, NSDictionary *indicesToKeys);
 
--- src/Core/OONSOperation.h
+++ src/Core/OONSOperation.h
@@ -1,10 +1,34 @@
-/*	NSOperationQueue was introduced in Mac OS X 10.5 and GNUstep 0.19.x
-	(the current unstable series at the time of writing).
-	
-	This header helps us use it if it's available at runtime without requiring
-	it (see OOTextureLoadOperationQueueDispatcher).
-	-- Ahruman 2009-09-04
+/*
+ 
+NSOperationQueue was introduced in Mac OS X 10.5 and GNUstep 0.19.x.
+
+This header helps us use it if it's available at runtime without requiring
+it (see OOAsyncWorkManager.m).
+-- Ahruman 2009-09-04
+
+
+Copyright (C) 2009-2010 Jens Ayton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 */
+
 
 #ifndef OO_HAVE_NSOPERATION
 	#if OOLITE_MAC_OS_X
