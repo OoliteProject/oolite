66ad3928a74236a63bf5a3f7ea629adb6072bb2a
Jens Ayton <jens@oolite.org>
2010-07-25 12:33:06 +0000

    Fixed galactic hyperspace, completely disabled in r3467/r3469.
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@3708 127b21dd-08f5-0310-b4b7-95ae10353056

--- src/Core/Entities/PlayerEntity.m
+++ src/Core/Entities/PlayerEntity.m
@@ -2252,7 +2252,7 @@ static GLfloat		sBaseMass = 0.0;
 			[self doScriptEvent:@"playerJumpFailed" withArgument:@"insufficient fuel"];
 			go = NO;
 		}
-		if ((![UNIVERSE inInterstellarSpace]) && equal_seeds(system_seed,target_system_seed))
+		if (!galactic_witchjump && ![UNIVERSE inInterstellarSpace] && equal_seeds(system_seed,target_system_seed))
 		{
 			//dont allow player to hyperspace to current location.  
 			//Note interstellar space will have a system_seed place we came from 
