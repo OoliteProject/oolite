df33517045037c7a0f7da77be85d67371ef2bbd3
Jens Ayton <jens@oolite.org>
2011-01-24 16:35:51 +0000

    Fix type mismatch.
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@4141 127b21dd-08f5-0310-b4b7-95ae10353056

--- src/Core/Entities/ShipEntity.m
+++ src/Core/Entities/ShipEntity.m
@@ -12,7 +12,7 @@ as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
+but WITHOUT ANY WARRANTY; without even the impllied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
@@ -9874,7 +9874,7 @@ static BOOL AuthorityPredicate(Entity *entity, void *parameter)
 #endif
 
 
-- (OOScript *)script
+- (OOJSScript *)script
 {
 	return script;
 }
