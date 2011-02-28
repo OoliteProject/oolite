c703289b4a92411d1fee321dd9facba620ebb5dd
Jens Ayton <jens@oolite.org>
2011-02-28 23:25:54 +0000

    Mac 32-bit build fix.
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@4423 127b21dd-08f5-0310-b4b7-95ae10353056

--- src/Core/Entities/PlayerEntityLegacyScriptEngine.m
+++ src/Core/Entities/PlayerEntityLegacyScriptEngine.m
@@ -1196,7 +1196,12 @@ static int scriptRandomSeed = -1;	// ensure proper random function
 {
 	if (scriptTarget != self)  return;
 	
-	int64_t award = 10 * [valueString longLongValue];
+	/*	We can't use -longLongValue here for Mac OS X 10.4 compatibility, but
+		we don't need to since larger values have never been supported for
+		legacy scripts.
+	*/
+	int64_t award = [valueString intValue];
+	award *= 10;
 	if (award < 0 && credits < (OOCreditsQuantity)-award)  credits = 0;
 	else  credits += award;
 }
