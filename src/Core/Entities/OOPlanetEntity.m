2b2b3afe520567ff90f2fdae42fd90416ae04671
Chris Morris <cim@oolite.org>
2012-09-27 17:26:10 +0000

    Try re-enabling planet culling. Seems stable enough in tests.
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@5369 127b21dd-08f5-0310-b4b7-95ae10353056

--- src/Core/Entities/OOPlanetEntity.m
+++ src/Core/Entities/OOPlanetEntity.m
@@ -478,13 +478,11 @@ static OOColor *ColorWithHSBColor(Vector c)
 	if (translucent || [UNIVERSE breakPatternHide])   return; // DON'T DRAW
 	if (_miniature && ![self isFinishedLoading])  return; // For responsiveness, don't block to draw as miniature.
 	
-#ifdef RAINBOW_DUST_INVESTIGATION
 	if (![UNIVERSE viewFrustumIntersectsSphereAt:position withRadius:([self radius] + ATMOSPHERE_DEPTH)])
 	{
 		// Don't draw
 		return;
 	}
-#endif
 	
 	if ([UNIVERSE wireframeGraphics])  GLDebugWireframeModeOn();
 	
