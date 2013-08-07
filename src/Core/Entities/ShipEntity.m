a7c030541f40edb4d90f0d05bb68f1b5170add54
Nikos Barkas <another_cmdr@oolite.org>
2013-08-07 20:36:24 +0000

    Reverted the oops of the previous commit and applied correct fix to subentities rotational_velocity issue as per cim's suggestion.
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk@5689 127b21dd-08f5-0310-b4b7-95ae10353056

--- src/Core/Entities/ShipEntity.m
+++ src/Core/Entities/ShipEntity.m
@@ -2323,7 +2323,7 @@ ShipEntity* doOctreesCollide(ShipEntity* prime, ShipEntity* other)
 		qf.x *= delta_t;
 		qf.y *= delta_t;
 		qf.z *= delta_t;
-		orientation = quaternion_multiply(qf, orientation);
+		[self setOrientation:quaternion_multiply(qf, orientation)];
 	}
 	
 	//	reset totalBoundingBox
@@ -2367,6 +2367,10 @@ ShipEntity* doOctreesCollide(ShipEntity* prime, ShipEntity* other)
 		break;
 
 	case BEHAVIOUR_IDLE :
+		if ([self isSubEntity])
+		{
+			applyThrust = NO;
+		}
 		[self behaviour_idle: delta_t];
 		break;
 
