0b3ec5b2161da26640d0bab3a64dbb0ed1af7f75
cim <cim@oolite.org>
2013-08-07 17:09:47 +0100

    Fix infinite loop typo

--- Resources/Scripts/oolite-ailib.js
+++ Resources/Scripts/oolite-ailib.js
@@ -3053,7 +3053,7 @@ AILib.prototype.configurationSelectRandomTradeStation = function()
 		}
 	}
 	var friendlies = 0;
-	for (var i = stations.length -1 ; i >= 0 ; i++)
+	for (var i = stations.length -1 ; i >= 0 ; i--)
 	{
 		var station = stations[i];
 		if (this.friendlyStation(station))
