a1993755b3fca3441467cb7d6ee0612b0b3b0102
Kevin Anthoney <kevin@kanthoney.co.uk>
2013-11-08 22:57:17 +0000

    nonlinear joystick patch

--- src/Core/Entities/PlayerEntityControls.m
+++ src/Core/Entities/PlayerEntityControls.m
@@ -2904,16 +2904,6 @@ static NSTimeInterval	time_last_frame;
 	NSUInteger			numSticks = [stickHandler joystickCount];
 	NSPoint				virtualStick = NSZeroPoint;
 	double				reqYaw = 0.0;
-	double				deadzone;
-	
-	if (mouse_control_on)
-	{
-		deadzone = 0.0;
-	}
-	else
-	{
-		deadzone = STICK_DEADZONE / [stickHandler getSensitivity];
-	}
 	
 	/*	DJS: Handle inputs on the joy roll/pitch axis.
 	 Mouse control on takes precidence over joysticks.
@@ -2931,9 +2921,9 @@ static NSTimeInterval	time_last_frame;
 	{
 		virtualStick = [stickHandler rollPitchAxis];
 		// handle roll separately (fix for BUG #17490)
-		if((virtualStick.x == STICK_AXISUNASSIGNED) || (fabs(virtualStick.x) < deadzone))
+		if(virtualStick.x == STICK_AXISUNASSIGNED)
 		{
-			// Not assigned or deadzoned - set to zero.
+			// Not assigned - set to zero.
 			virtualStick.x=0;
 		}
 		else if(virtualStick.x != 0)
@@ -2942,9 +2932,9 @@ static NSTimeInterval	time_last_frame;
 			keyboardRollOverride=NO;
 		}
 		// handle pitch separately (fix for BUG #17490)
-		if((virtualStick.y == STICK_AXISUNASSIGNED) || (fabs(virtualStick.y) < deadzone))
+		if(virtualStick.y == STICK_AXISUNASSIGNED)
 		{
-			// Not assigned or deadzoned - set to zero.
+			// Not assigned - set to zero.
 			virtualStick.y=0;
 		}
 		else if(virtualStick.y != 0)
@@ -2954,7 +2944,7 @@ static NSTimeInterval	time_last_frame;
 		}
 		// handle yaw separately from pitch/roll
 		reqYaw = [stickHandler getAxisState: AXIS_YAW];
-		if((reqYaw == STICK_AXISUNASSIGNED) || fabs(reqYaw) < deadzone)
+		if(reqYaw == STICK_AXISUNASSIGNED)
 		{
 			// Not assigned or deadzoned - set to zero.
 			reqYaw=0;
@@ -3015,7 +3005,7 @@ static NSTimeInterval	time_last_frame;
 			if (flightRoll < stick_roll)
 				flightRoll = stick_roll;
 		}
-		rolling = (fabs(virtualStick.x) >= deadzone);
+		rolling = (fabs(virtualStick.x) > 0.0);
 	}
 	if (!rolling)
 	{
@@ -3070,7 +3060,7 @@ static NSTimeInterval	time_last_frame;
 			if (flightPitch < stick_pitch)
 				flightPitch = stick_pitch;
 		}
-		pitching = (fabs(virtualStick.y) >= deadzone);
+		pitching = (fabs(virtualStick.y) > 0.0);
 	}
 	if (!pitching)
 	{
@@ -3127,7 +3117,7 @@ static NSTimeInterval	time_last_frame;
 			if (flightYaw < stick_yaw)
 				flightYaw = stick_yaw;
 		}
-		yawing = (fabs(reqYaw) >= deadzone);
+		yawing = (fabs(reqYaw) > 0.0);
 	}
 	if (!yawing)
 	{
