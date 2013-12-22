66788a94899d1b815af07296e1ca9947125f6fc1
cim <cim@oolite.org>
2013-12-22 15:14:46 +0000

    Allow scenarios to end while docked.

--- src/Core/Entities/PlayerEntity.m
+++ src/Core/Entities/PlayerEntity.m
@@ -10388,7 +10388,9 @@ else _dockTarget = NO_TARGET;
         case STATUS_START_GAME:
             isDockedStatus = YES;
             break;   
+			// special case - can be either docked or not, so avoid safety check below
 		case STATUS_RESTART_GAME:
+			return NO;
 		case STATUS_EFFECT:
 		case STATUS_ACTIVE:
 		case STATUS_COCKPIT_DISPLAY:
