d1dd01fb856c1102125eb895a1e8c8fe56609f5b
C.W. Betts <computers57@hotmail.com>
2015-05-09 22:44:57 -0600

    Revert some changes that use their own custom enumerators.

--- src/Core/Entities/PlayerEntityContracts.m
+++ src/Core/Entities/PlayerEntityContracts.m
@@ -1933,10 +1933,11 @@ static NSMutableDictionary *currentShipyard = nil;
 	// keep track of portable equipment..
 
 	NSMutableSet	*portable_equipment = [NSMutableSet set];
+	NSEnumerator	*eqEnum = nil;
 	NSString		*eq_desc = nil;
 	OOEquipmentType	*item = nil;
 	
-	foreach (eq_desc, [self equipmentEnumerator])
+	for (eqEnum = [self equipmentEnumerator]; (eq_desc = [eqEnum nextObject]);)
 	{
 		item = [OOEquipmentType equipmentTypeWithIdentifier:eq_desc];
 		if ([item isPortableBetweenShips])  [portable_equipment addObject:eq_desc];
@@ -1946,7 +1947,7 @@ static NSMutableDictionary *currentShipyard = nil;
 	[self removeAllEquipment];
 	
 	// restore  portable equipment
-	foreach (eq_desc, portable_equipment)
+	for (eqEnum = [portable_equipment objectEnumerator]; (eq_desc = [eqEnum nextObject]); )
 	{
 		[self addEquipmentItem:eq_desc withValidation:NO inContext:@"portable"];
 	}
