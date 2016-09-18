0fdebf57c33764515fc79d13cd5f01152d6a4c41
AnotherCommander <another_commander@oolite.org>
2016-09-18 13:20:11 +0200

    New atmospheres applied on detail level Extras, original atmospheres below that.

--- src/Core/Entities/OOPlanetEntity.m
+++ src/Core/Entities/OOPlanetEntity.m
@@ -830,47 +830,49 @@ static OOColor *ColorWithHSBColor(Vector c)
 	/* Generate atmosphere texture */
 	if (!isMoon)
 	{
-#if OO_SHADERS
-		NSMutableDictionary *aConfig = [[[materialDefaults oo_dictionaryForKey:@"atmosphere-material"] mutableCopy] autorelease];
-		[aConfig setObject:[NSArray arrayWithObjects:diffuseMap, normalMap, nil] forKey:@"_oo_texture_objects"];
-		
-		NSDictionary *amacros = [materialDefaults oo_dictionaryForKey:@"atmosphere-dynamic-macros"];
-		
-		OOMaterial *dynamicMaterial = [OOShaderMaterial shaderMaterialWithName:@"dynamic"
-																configuration:aConfig
-																macros:amacros
-																bindingTarget:self];
-																
-		if (dynamicMaterial == nil)
+		if (shadersOn)
 		{
-			/* Backup plan */
+			NSMutableDictionary *aConfig = [[[materialDefaults oo_dictionaryForKey:@"atmosphere-material"] mutableCopy] autorelease];
+			[aConfig setObject:[NSArray arrayWithObjects:diffuseMap, normalMap, nil] forKey:@"_oo_texture_objects"];
+			
+			NSDictionary *amacros = [materialDefaults oo_dictionaryForKey:@"atmosphere-dynamic-macros"];
+			
+			OOMaterial *dynamicMaterial = [OOShaderMaterial shaderMaterialWithName:@"dynamic"
+																	configuration:aConfig
+																	macros:amacros
+																	bindingTarget:self];
+																	
+			if (dynamicMaterial == nil)
+			{
+				/* Backup plan */
+				OOLog(@"texture.planet.generate",@"Preparing atmosphere for planet %@",self);
+				/* Generate a standalone atmosphere texture */
+				OOTexture *atmosphere = nil;
+				[OOStandaloneAtmosphereGenerator generateAtmosphereTexture:&atmosphere
+										withInfo:_materialParameters];
+			
+				OOLog(@"texture.planet.generate",@"Planet %@ has atmosphere %@",self,atmosphere);
+				dynamicMaterial = [[OOSingleTextureMaterial alloc] initWithName:@"dynamic"
+															texture:atmosphere configuration:nil];
+				[dynamicMaterial autorelease];
+			}
+			
+			[_atmosphereDrawable setMaterial:dynamicMaterial];
+		}
+		else
+		{
 			OOLog(@"texture.planet.generate",@"Preparing atmosphere for planet %@",self);
 			/* Generate a standalone atmosphere texture */
 			OOTexture *atmosphere = nil;
 			[OOStandaloneAtmosphereGenerator generateAtmosphereTexture:&atmosphere
-									withInfo:_materialParameters];
-		
+															withInfo:_materialParameters];
+			
 			OOLog(@"texture.planet.generate",@"Planet %@ has atmosphere %@",self,atmosphere);
-			dynamicMaterial = [[OOSingleTextureMaterial alloc] initWithName:@"dynamic"
-														texture:atmosphere configuration:nil];
-			[dynamicMaterial autorelease];
+			
+			OOSingleTextureMaterial *dynamicMaterial = [[OOSingleTextureMaterial alloc] initWithName:@"dynamic" texture:atmosphere configuration:nil];
+			[_atmosphereDrawable setMaterial:dynamicMaterial];
+			[dynamicMaterial release];
 		}
-		
-		[_atmosphereDrawable setMaterial:dynamicMaterial];
-
-#else
-		OOLog(@"texture.planet.generate",@"Preparing atmosphere for planet %@",self);
-		/* Generate a standalone atmosphere texture */
-		OOTexture *atmosphere = nil;
-		[OOStandaloneAtmosphereGenerator generateAtmosphereTexture:&atmosphere
-														  withInfo:_materialParameters];
-		
-		OOLog(@"texture.planet.generate",@"Planet %@ has atmosphere %@",self,atmosphere);
-		
-		OOSingleTextureMaterial *dynamicMaterial = [[OOSingleTextureMaterial alloc] initWithName:@"dynamic" texture:atmosphere configuration:nil];
-		[_atmosphereDrawable setMaterial:dynamicMaterial];
-		[dynamicMaterial release];
-#endif
 	}
 
 	OOMaterial *material = nil;
