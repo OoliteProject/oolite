f930b3a9671a4c01d6ffc539c2996140aa0625fb
AnotherCommander <nikbar2004@gmail.com>
2024-06-14 13:40:55 +0300

    Fixed Arch Linux compile error with GCC 14.
    Closes #484

--- src/Core/OOPolygonSprite.m
+++ src/Core/OOPolygonSprite.m
@@ -115,6 +115,9 @@ static void APIENTRY TessEndCallback(void *polygonData);
 
 static void APIENTRY ErrorCallback(GLenum error, void *polygonData);
 
+// this is needed to maintain compatibility with GCC 14+
+typedef GLvoid (*TessFuncPtr)();
+
 
 @implementation OOPolygonSprite
 
@@ -283,11 +286,11 @@ static void APIENTRY ErrorCallback(GLenum error, void *polygonData);
 	dataArray = DataArrayToPoints(&polygonData, dataArray);
 	
 	/*** Tesselate polygon fill ***/
-	gluTessCallback(tesselator, GLU_TESS_BEGIN_DATA, TessBeginCallback);
-	gluTessCallback(tesselator, GLU_TESS_VERTEX_DATA, TessVertexCallback);
-	gluTessCallback(tesselator, GLU_TESS_END_DATA, TessEndCallback);
-	gluTessCallback(tesselator, GLU_TESS_ERROR_DATA, ErrorCallback);
-	gluTessCallback(tesselator, GLU_TESS_COMBINE_DATA, TessCombineCallback);
+	gluTessCallback(tesselator, GLU_TESS_BEGIN_DATA, (TessFuncPtr)TessBeginCallback);
+	gluTessCallback(tesselator, GLU_TESS_VERTEX_DATA, (TessFuncPtr)TessVertexCallback);
+	gluTessCallback(tesselator, GLU_TESS_END_DATA, (TessFuncPtr)TessEndCallback);
+	gluTessCallback(tesselator, GLU_TESS_ERROR_DATA, (TessFuncPtr)ErrorCallback);
+	gluTessCallback(tesselator, GLU_TESS_COMBINE_DATA, (TessFuncPtr)TessCombineCallback);
 	
 	gluTessBeginPolygon(tesselator, &polygonData);
 	SVGDumpBeginGroup(&polygonData, @"Fill");
@@ -351,11 +354,11 @@ static void APIENTRY ErrorCallback(GLenum error, void *polygonData);
 	polygonData.generatingOutline = YES;
 #endif
 	
-	gluTessCallback(tesselator, GLU_TESS_BEGIN_DATA, TessBeginCallback);
-	gluTessCallback(tesselator, GLU_TESS_VERTEX_DATA, TessVertexCallback);
-	gluTessCallback(tesselator, GLU_TESS_END_DATA, TessEndCallback);
-	gluTessCallback(tesselator, GLU_TESS_ERROR_DATA, ErrorCallback);
-	gluTessCallback(tesselator, GLU_TESS_COMBINE_DATA, TessCombineCallback);
+	gluTessCallback(tesselator, GLU_TESS_BEGIN_DATA, (TessFuncPtr)TessBeginCallback);
+	gluTessCallback(tesselator, GLU_TESS_VERTEX_DATA, (TessFuncPtr)TessVertexCallback);
+	gluTessCallback(tesselator, GLU_TESS_END_DATA, (TessFuncPtr)TessEndCallback);
+	gluTessCallback(tesselator, GLU_TESS_ERROR_DATA, (TessFuncPtr)ErrorCallback);
+	gluTessCallback(tesselator, GLU_TESS_COMBINE_DATA, (TessFuncPtr)TessCombineCallback);
 	gluTessProperty(tesselator, GLU_TESS_WINDING_RULE, GLU_TESS_WINDING_POSITIVE);
 	
 	gluTessBeginPolygon(tesselator, &polygonData);
