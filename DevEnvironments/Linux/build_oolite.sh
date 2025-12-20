85c162d47b5c340804686316d9d2c75d6f77eb75
mcarans <rans@email.com>
2025-12-20 17:40:52 +1300

    Missed adding this

---
+++ DevEnvironments/Linux/build_oolite.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# No parameters: build target = release
+# One parameter: build target
+
+run_script() {
+    # First optional parameter is build target. Default target is release.
+    SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+    pushd "$SCRIPT_DIR"
+
+    export CC=clang
+    export CXX=clang++
+
+    cd ../../oolite
+
+    if [[ -z "$1" ]]; then
+        TARGET=release
+    else
+        TARGET=$1
+    fi
+
+	if make -f Makefile $TARGET -j$(nproc); then
+		echo "✅ Oolite build completed successfully"
+	else
+		echo "❌ Oolite build failed" >&2
+		return 1
+	fi
+
+	popd
+}
+
+run_script "$@"
+status=$?
+
+
+# Exit only if not sourced
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    exit $status
+fi
+
