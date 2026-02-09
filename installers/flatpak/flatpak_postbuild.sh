8ac16e988f35d1e7e47af4999daf2f8cf95b33e5
mcarans <rans@email.com>
2026-02-09 14:01:07 +1300

    Further changes requested by FlatHub

---
+++ installers/flatpak/flatpak_postbuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cp -r oolite.app/* /app/bin
+source ShellScripts/common/get_version.sh
+
+FLATPAK_METAINFO=/app/share/metainfo/space.oolite.Oolite.metainfo.xml
+install -D installers/flatpak/space.oolite.Oolite.metainfo.xml $FLATPAK_METAINFO
+sed -i "/<releases>/a \\    <release version=\"${VER}\" date=\"$(date -I)\" />" $FLATPAK_METAINFO
+
+FLATPAK_DESKTOP=/app/share/applications/space.oolite.Oolite.desktop
+install -D installers/FreeDesktop/oolite.desktop $FLATPAK_DESKTOP
+desktop-file-edit --set-key=Exec --set-value=run_oolite.sh $FLATPAK_DESKTOP
+desktop-file-edit --set-key=Icon --set-value=space.oolite.Oolite $FLATPAK_DESKTOP
+
+install -D Oolite-logo3.png /app/share/icons/hicolor/256x256/apps/space.oolite.Oolite.png
