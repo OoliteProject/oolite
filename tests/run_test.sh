e2cef2236699d96a875faf619ced161546594f82
mcarans <rans@email.com>
2026-04-06 20:08:14 +1200

    Try ucrt64 mesa

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -14,6 +14,16 @@ run_script() {
     SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
     pushd "$SCRIPT_DIR"
 
+    if [[ -n "$MSYSTEM" ]]; then
+        MESA_DLL="${MSYSTEM_PREFIX}/bin/opengl32.dll"
+        TARGET_DIR="../oolite.app"
+
+        if [[ -f "$MESA_DLL" ]]; then
+            echo "📦 Found $MSYSTEM Mesa driver at $MESA_DLL"
+            cp "$MESA_DLL" "$TARGET_DIR/"
+        fi
+    fi
+
     if ! $PYTHON_CMD launch_snapshot.py --path=../oolite.app; then
         echo "❌ Oolite test failed!" >&2
         return 1
