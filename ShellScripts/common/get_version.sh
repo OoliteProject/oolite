859c34b350f4d227856dac142100661ef0e1bea1
cube <cube@github.com>
2026-05-10 17:44:33 +0200

    pass semver to get_version.sh script

--- ShellScripts/common/get_version.sh
+++ ShellScripts/common/get_version.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Calculates the Oolite version number if not passed via env variables.
+# Output goes into the OOLITE_VERSION.txt file and to stdout.
+#
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 pushd "$SCRIPT_DIR" > /dev/null
@@ -6,30 +10,37 @@ pushd "$SCRIPT_DIR" > /dev/null
 mkdir -p ../../build
 cd ../../build
 
-VERSION=$(cat ../src/Cocoa/oolite-version.xcconfig | cut -d '=' -f 2)
-VER_MAJ=$(echo "$VERSION" | cut -d. -f1)
-VER_MIN=$(echo "$VERSION" | cut -d. -f2)
-VER_REV=$(echo "$VERSION" | cut -d. -f3)
-if [ "" == "$VER_REV" ]; then
-    VER_REV="0"
+if [ -z "${SEMVER}" ] || [ -z "${PROJECTNAME}" ]
+then
+    # Variables not passed in. Calculate the classic way.
+
+    VERSION=$(cat ../src/Cocoa/oolite-version.xcconfig | cut -d '=' -f 2)
+    VER_MAJ=$(echo "$VERSION" | cut -d. -f1)
+    VER_MIN=$(echo "$VERSION" | cut -d. -f2)
+    VER_REV=$(echo "$VERSION" | cut -d. -f3)
+    if [ "" == "$VER_REV" ]; then
+        VER_REV="0"
+    fi
+    TIMESTAMP=$(git log -1 --format=%ct)
+    # Date conversions use UTC for consistency
+    # Convert to __DATE__ format (e.g., Feb 20 2026)
+    CPP_DATE=$(date -u -d "@$TIMESTAMP" +"%b %e %Y")
+    # Convert to YYYY-MM-DD
+    APP_DATE=$(date -u -d "@$TIMESTAMP" +"%Y-%m-%d")
+    # Convert to YYMMDD format (e.g., 260313)
+    VER_DATE=$(date -u -d "@$TIMESTAMP" +"%y%m%d")
+
+    VER_GITREV=$(git rev-list --count HEAD)
+    VER_GITHASH=$(git rev-parse --short=7 HEAD)
+    VER_FULL="$VER_MAJ.$VER_MIN.$VER_REV.$VER_GITREV-$VER_DATE-$VER_GITHASH"
+    BUILDTIME=$(date "+%Y.%m.%d %H:%M")
+else
+    # Variables passed in. Make use of them.
+
+    VER_FULL="${SEMVER}"
 fi
-TIMESTAMP=$(git log -1 --format=%ct)
-# Date conversions use UTC for consistency
-# Convert to __DATE__ format (e.g., Feb 20 2026)
-CPP_DATE=$(date -u -d "@$TIMESTAMP" +"%b %e %Y")
-# Convert to YYYY-MM-DD
-APP_DATE=$(date -u -d "@$TIMESTAMP" +"%Y-%m-%d")
-# Convert to YYMMDD format (e.g., 260313)
-VER_DATE=$(date -u -d "@$TIMESTAMP" +"%y%m%d")
-
-VER_GITREV=$(git rev-list --count HEAD)
-VER_GITHASH=$(git rev-parse --short=7 HEAD)
-VER_FULL="$VER_MAJ.$VER_MIN.$VER_REV.$VER_GITREV-$VER_DATE-$VER_GITHASH"
-BUILDTIME=$(date "+%Y.%m.%d %H:%M")
-
 
 echo "OOLITE_VERSION=$VER_FULL" > OOLITE_VERSION.txt
-
 echo "$VER_FULL"
 
 popd > /dev/null
